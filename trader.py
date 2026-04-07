6ee4332f9d56dd3ab7899245ffa2cfa10bf82f66
researcher <researcher@autoresearch>
2026-04-07 01:55:02 +0000

    exp: revert COCONUT edge to 2.0

--- trader.py
+++ trader.py
@@ -457,7 +457,7 @@ class Trader:
             # P2: use fixed sigma for stable pricing
             fixed_sigma = 0.194
             fair = bs_call_price(S, strike, T, fixed_sigma)
-            edge_thr = 1.5  # tighter edge for P2 coconut coupon
+            edge_thr = 2.0  # tighter edge for P2 coconut coupon
         else:
             # P3: use fitted vol smile (FrankfurtHedgehogs approach)
             # coeffs from fitted volatility smile: IV = f(log(K/S)/sqrt(T))
