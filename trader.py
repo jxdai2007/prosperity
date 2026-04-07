2aa7724cb635bb13b52e1bd0981aee9242b9a079
researcher <researcher@autoresearch>
2026-04-07 00:57:38 +0000

    exp: tighter dynamic MM taking (0.5->1 edge)

--- trader.py
+++ trader.py
@@ -267,9 +267,9 @@ class Trader:
         pos = self.get_position(product, state)
         spread = MM_DYNAMIC_SPREAD
 
-        # Take mispriced orders
+        # Take mispriced orders (must beat fair by at least 1)
         for ask_price in sorted(od.sell_orders.keys()):
-            if ask_price < fair - 0.5:
+            if ask_price < fair - 1:
                 ask_vol = -od.sell_orders[ask_price]
                 can_buy = limit - pos
                 qty = min(ask_vol, can_buy)
@@ -278,7 +278,7 @@ class Trader:
                     pos += qty
 
         for bid_price in sorted(od.buy_orders.keys(), reverse=True):
-            if bid_price > fair + 0.5:
+            if bid_price > fair + 1:
                 bid_vol = od.buy_orders[bid_price]
                 can_sell = limit + pos
                 qty = min(bid_vol, can_sell)
