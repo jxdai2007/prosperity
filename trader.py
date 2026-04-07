403fa6e31f2b049156a9a6d7798321bc69a137b0
researcher <researcher@autoresearch>
2026-04-07 05:45:44 +0000

    exp: basket qty 5->3

--- trader.py
+++ trader.py
@@ -356,7 +356,7 @@ class Trader:
         basket_pos = self.get_position(basket, state)
         basket_od = state.order_depths[basket]
         basket_orders = []
-        max_qty = 5  # optimal basket qty
+        max_qty = 3  # test smaller qty
 
         if deviation > entry_thr:
             # Basket expensive -> sell basket aggressively
